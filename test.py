d391511b881705e289062d876116adad750510e7
Parekh Parth Vijaykumar <parth.innovator@gmail.com>
2020-08-20 17:07:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import rtsp
 
-client = rtsp.client(rtsp_server_uri = 'rtsp://admin:Cimcon123@199.199.51.203:554/mpeg/ch1/sub/av_stream')
+client = rtsp.Client(rtsp_server_uri = 'rtsp://admin:Cimcon123@199.199.51.203:554/mpeg/ch1/sub/av_stream')
 test = client.read()
 print(test)
 client.close()
